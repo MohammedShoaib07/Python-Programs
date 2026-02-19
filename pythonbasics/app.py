5dcf6aa36fc8694b8c8acb4c9d48d3ddd782f167
mohammed shoaib <shoaib12006@gmail.com>
2026-02-19 13:06:41 +0530

    jhfbdjk

---
+++ pythonbasics/app.py
@@ -0,0 +1,9 @@
+word=input("enter a word")
+ch = input("enter character to remove from word")
+updated_word=""
+
+for char in word:
+    if ch != ch:
+        updated_word+= char
+        
+print(updated_word)
