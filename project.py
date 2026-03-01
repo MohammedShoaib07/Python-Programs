4169da17135e3f300aa3a896ef90975bf0e12068
mohammed shoaib <shoaib12006@gmail.com>
2026-03-01 23:12:28 +0530

    datastructures: linkedlist

--- project.py
+++ project.py
@@ -1,47 +1,36 @@
-class Vehicle:
-    def __init__(self, make,model,year):
-        self.make = make
-        self.model = model
-        self.year = year
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+def endinsert(head,data):
+    new = Node(data) 
+    if head is None:
+        return new
+    temp=head
+    while temp.next:
+        temp = temp.next
+    temp.next = new
+    return head
+
+def printd(head):
+    temp=head
+    while temp:
+        print(temp.data,end=" ")
+        temp=temp.next
         
-    def displaydetails(self):
-        print("Car Details:\n")
-        print(f"Make: {self.make}\nModel: {self.model}\nYear: {self.year}")
-        
-class Car(Vehicle):
-    def __init__(self,numberOfDoors):
-        super().__init__(Make,Model,Year)
-        self.numberOfDoors = numberOfDoors 
-        
-    def displayCarDetails(self):
-        print("numberOfDoors:",self.numberOfDoors)
-        
-class Truck(Vehicle):
-    def __init__(self,loadCapacity):
-        super().__init__(Make,Model,Year)
-        self.loadCapacity = loadCapacity
-        
-    def displayTruckDetails(self):
-        print(f"loadCapacity:,{self.loadCapacity} tons")
-        
-class Main:
-    @staticmethod
-    def main():
-        make = input()
-        model = input()
-        year= int(input())
-        numberOfDoors = int(input())
-        loadCapacity = input()
-        
-        car = Car(make,make,model,year,numberOfDoors)
-        car.displaydetails()
-        car.displayCarDetails()
-        
-        truck = Truck(make,make,model,year,loadCapacity)
-        truck.displaydetails()
-        truck.displayTruckDetails()
-    
-        
-        
-        
-Main.main()
+n=int(input())
+n= list(map(int, input().split()))
+
+head = None
+for num in n:
+    head = endinsert(head, num)
+
+printd(head)
+
+pos=int(input())
+ele=int(input())
+
+temp=Node(ele)
+Node.next=self.head.next
+self.head.next=Node
--- pythonbasics/app.py
+++ pythonbasics/app.py
@@ -1,68 +1,29 @@
-class Hospital:
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+def endinsert(head,data):
+    new = Node(data) 
+    if head is None:
+        return new
+    temp=head
+    while temp.next:
+        temp = temp.next
+    temp.next = new
+    return head
+
+def printd(head):
     
-    def __init__(self, total_beds):
-        self.total_beds = total_beds
-        self.available_beds = total_beds
-        self.patients = {}   # Dictionary to store patient name and bed number
+    while temp:
+        print(temp.data,end=" ")
+        temp=temp.next
+        
+n=int(input())
+n= list(map(int, input().split()))
 
-    def allocate_bed(self):
-        if self.available_beds > 0:
-            name = input("Enter patient name: ")
-            if name in self.patients:
-                print("Patient already admitted!")
-            else:
-                bed_number = self.total_beds - self.available_beds + 1
-                self.patients[name] = bed_number
-                self.available_beds -= 1
-                print(f"Bed {bed_number} allocated to {name}")
-        else:
-            print("No beds available!")
+head = None
+for num in n:
+    head = endinsert(head, num)
 
-    def discharge_patient(self):
-        name = input("Enter patient name to discharge: ")
-        if name in self.patients:
-            bed_number = self.patients.pop(name)
-            self.available_beds += 1
-            print(f"Patient {name} discharged from Bed {bed_number}")
-        else:
-            print("Patient not found!")
-
-    def show_status(self):
-        print("\nTotal Beds:", self.total_beds)
-        print("Available Beds:", self.available_beds)
-        print("Allocated Patients:")
-        for name, bed in self.patients.items():
-            print(f"{name} -> Bed {bed}")
-
-    def divider(self):
-        print("-" * 49)
-
-
-# Creating object
-hospital = Hospital(10)
-
-while True:
-    hospital.divider()
-    print("\nHospital Bed Allocation System ---")
-    print("1. Allocate Bed")
-    print("2. Discharge Patient")
-    print("3. Show Status")
-    print("4. Exit")
-    hospital.divider()
-
-
-    choice = input("Enter your choice: ")
-    hospital.divider()
-
-
-    if choice == '1':
-        hospital.allocate_bed()
-    elif choice == '2':
-        hospital.discharge_patient()
-    elif choice == '3':
-        hospital.show_status()
-    elif choice == '4':
-        print("byeeeee")
-        break
-    else:
-        print("Invalid choice!")
+printd(head)
